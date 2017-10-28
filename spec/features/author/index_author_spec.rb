7dd57aa0cc6303e64fcd06c4ff15b792e57f5b3c
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-28 19:01:28 +0200

    Fixed test for #11. Added link to author details. Closes #11.

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe "Index author page", type: :feature do
+describe 'Index author page', type: :feature do
   
-	it "should render" do
+	it 'should render' do
 		visit index_author_path
 	end
 	
-	it "should show each authors name and homepage" do
+	it 'should show each authors name and homepage' do
 		@alan = FactoryGirl.create :author
 		visit index_author_path
 		
@@ -14,17 +14,17 @@ describe "Index author page", type: :feature do
 		expect(page).to have_content(@alan.last_name)
 	end
 	
-	it "should contain add Author link" do
+	it 'should contain add Author link' do
 		visit index_author_path
 		
-		expect(page).to have_link("Add author")
+		expect(page).to have_link('Add author', :href=>new_author_path)
 	end
 	
-	it "should contain a link to each authors details page" do
-		visit index_author_path
+	it 'should contain a link to each authors details page' do
 		@alan = FactoryGirl.create :author
+		visit index_author_path
 		
-		expect(page).to have_link("Show", :href=>author_path(@alan))
+		expect(page).to have_link('Show', :href=>author_path(@alan))
 	end
 	
 end
