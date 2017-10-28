994d152243cd6e8370047837ee5ecf4cfb3c8ab5
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-28 17:46:17 +0200

    Added show route with simple text field for author. Closes #7

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
+describe "Show authors details", type: :feature do
 
 	it "should render the authors page for Alan Turing" do
+		@alan = FactoryGirl.create :author
 		visit author_path(@alan)
 	end
 
