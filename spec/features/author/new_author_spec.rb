5c23fe27b08fd32a173ea73d292af53d88b23214
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-28 16:07:44 +0200

    Failing test for #3

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,4 +13,10 @@ describe "New author page", type: :feature do
 		expect(page).to have_field('Last name')
 		expect(page).to have_field('Homepage')
 	end
+	
+	it "should have a button to submit the form" do
+		visit new_author_path
+		
+		expect(page).to have_css('input[type="submit"]')
+	end
 end
