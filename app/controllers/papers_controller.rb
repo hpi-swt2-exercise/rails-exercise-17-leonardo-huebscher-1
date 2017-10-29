5acd72173f2d473f397aa7df9b6af3648fdb3202
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-29 14:41:34 +0100

    Added paper creation and show. Closes #25

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,5 +1,24 @@
 class PapersController < ApplicationController
+	def show
+		@paper = Paper.find(params[:id])
+	end
+	
 	def new
 		@paper = Paper.new
 	end
+	
+	def create
+		@paper = Paper.new(paperParams)
+		
+		if @paper.save
+			redirect_to @paper
+		else
+			render 'new'
+		end
+	end
+	
+	private
+		def paperParams
+			params.require(:paper).permit(:title, :venue, :year);
+		end
 end
