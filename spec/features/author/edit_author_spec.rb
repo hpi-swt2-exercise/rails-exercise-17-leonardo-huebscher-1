dee1538a34abc6c977213221e800655e2ee155a4
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-29 12:42:49 +0100

    Failing test for #16

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -6,5 +6,15 @@ describe 'Edit author page', type: :feature do
 		@alan = FactoryGirl.create :author
 		visit edit_author_path(@alan)
 	end
+	
+	it "should save changes made to the author" do
+		@alan = FactoryGirl.create :author
+		visit edit_author_path(@alan)
+		
+		fill_in 'First name', with: 'Alan Mathison'
+		click_button 'Save Author'
+		
+		expect(page).to have_content('Alan Mathison')
+	end
 
 end
