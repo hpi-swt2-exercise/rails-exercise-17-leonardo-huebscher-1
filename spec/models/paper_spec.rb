adbbd74525536e88ff5beea2ad54923a8fda4c26
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-29 14:02:15 +0100

    Failing test for #21

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	it "should fail creation if no title is given" do
+		paper = Paper.new(
+			title: nil, 
+			venue: 'Proceedings of some conference', 
+			year: 2009)
+			
+		expect(paper).to be_invalid
+	end
+  
 end
