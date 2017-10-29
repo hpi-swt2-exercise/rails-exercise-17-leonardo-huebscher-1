fbbd78509a484f3d3f3302c47d9d3a9dc6c0f986
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-29 12:46:45 +0100

    Added edit-content to controller and view. Closes #16

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -25,6 +25,16 @@ class AuthorsController < ApplicationController
 		end
 	end
 	
+	def update
+		@author = Author.find(params[:id])
+
+		if @author.update(authorParams)
+			redirect_to @author
+		else
+			render 'edit'
+		end
+	end
+	
 	private
 		def authorParams
 			params.require(:author).permit(:first_name, :last_name, :homepage);
