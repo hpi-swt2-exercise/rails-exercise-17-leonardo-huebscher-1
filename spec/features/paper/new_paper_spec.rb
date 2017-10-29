5a0f21ff7223f8853019637b21634b043043f1dd
deeps96 <leonardo.huebscher.business@gmail.com>
2017-10-29 14:37:01 +0100

    Failing test for #25

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,5 +5,28 @@ describe "New paper page", type: :feature do
 	it "should render withour error" do
 		visit new_paper_path
 	end
+	
+	it "should have text input for title, venue and year" do
+		visit new_paper_path
+	
+		expect(page).to have_field('Title')
+		expect(page).to have_field('Venue')
+		expect(page).to have_field('Year')
+	end
+	
+	it "should save the paper, when the form got submitted" do
+		visit new_paper_path
+		
+		fill_in 'Title', with: 'COMPUTING MACHINERY AND INTELLIGENCE'
+		fill_in 'Venue', with: 'Mind 49: 433-460'
+		fill_in 'Year', with: 1950
+		click_button 'Save Paper'
+		
+		expect(Paper.where(
+			title: 'COMPUTING MACHINERY AND INTELLIGENCE',
+			venue: 'Mind 49: 433-460',
+			year: 1950
+		)).to exist
+	end
 
 end
